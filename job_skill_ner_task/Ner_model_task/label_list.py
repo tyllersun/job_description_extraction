b1a93c7568d895e78f30a9d48befd062392a52c0
sunlidong <tylletsun@gmail.com>
2023-07-23 19:02:08 +0800

    Add: label list

---
+++ job_skill_ner_task/Ner_model_task/label_list.py
@@ -0,0 +1,5 @@
+label_list = [
+    'O',       # Outside of a named entity
+    'B-Skill',  # Beginning of a miscellaneous entity right after another miscellaneous entity
+    'I-Skill',  # Miscellaneous entity
+]
